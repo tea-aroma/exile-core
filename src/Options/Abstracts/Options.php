93cfa0583bdcccd15823f15ae7a6951a47c69690
tea-aroma <kovalyovictor71@gmail.com>
2025-06-26 10:42:38 +0500

    feat: add 'Options' class for centralized option management.

---
+++ src/Options/Abstracts/Options.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TeaAroma\ExileCore\Options\Abstracts;
+
+
+use TeaAroma\ExileCore\DataTransferObject\Abstracts\DataTransferObject;
+
+
+/**
+ * Provides the abstract logic for centralized option management.
+ */
+abstract class Options extends DataTransferObject {}
